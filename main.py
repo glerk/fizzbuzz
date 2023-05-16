3b7a7bec7da3cb0cd99ca0b5533c2e6c39c42482
jason <jasonpepin2000@gmail.com>
2023-05-15 20:01:50 -0400

    hi

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+#fizz buzz pracrice 
+
+#lst = list(range(0,99))
+
+def fizzbuzz():
+    for i in range(0,99):
+        print = "hello"
+
+__name__ == "__main__":
+    fizzbuzz()
